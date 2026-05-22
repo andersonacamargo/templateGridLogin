e1a45a88f82f5ff34306951698c1f180eace15c2
Aluno SENAI <Aluno@SN-404267.edu.fiesc.com.br>
2026-05-22 17:23:12 -0300

    inicio da integração

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -1,7 +1,28 @@
-import React from 'react';
-import "./popUp.css"
+import React, {useState} from 'react';
+import "./popUp.css";
+import axios from "axios";
+
 
 function PopUp({showPopUp, closePopUp}){
+  
+  const [logar, setLogar] = useState(false);
+
+  const postLogin = async () => {
+  try{
+    const res =  await axios.post('http://localhost:5000/registrar',{ 
+      nome:'anderson',
+      email:'test@tsest',
+      senha:'12345',
+      cargo:'prof'
+    },{
+      headers: {
+          'content-type': 'text/json'
+      }
+  })
+    console.log(res.status);
+    return res} 
+    catch (e){return null }
+  }
 
  
   if (!showPopUp) {return null}
@@ -11,7 +32,8 @@ function PopUp({showPopUp, closePopUp}){
         <input className='inputPopup' placeholder='login'></input>
         <label>Senha:</label>
         <input className='inputPopup' placeholder='senha'></input>
-        <div className="buttonLine"><button className='login' onClick={closePopUp}>Logar</button>
+        <div className="buttonLine">
+        <button className='login' onClick={() => postLogin()}>Logar</button>
         <button className='close' onClick={closePopUp}>Cancelar</button>
         </div>
     </div>
